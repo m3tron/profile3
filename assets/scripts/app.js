b1b2b65624c3457fe4fe36b0c459ade4352aeffe
Sunny Heyar <sunny.heyar@gmail.com>
2019-06-09 14:03:17 -0400

    replaced placeholders and added projects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,4 +4,33 @@ $(document).ready(function(e) {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
+
+  $(".top-nav .nav-link").on("click", () => {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+
+  $("nav a[href*='#']").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100
+      },
+      2000
+    );
+  });
+
+  $("#up").on("click", () => {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      2000
+    );
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true
+  });
 });
